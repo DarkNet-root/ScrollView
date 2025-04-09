a1331098af3f088a7348574fcc14a40a7f574826
DarkNet-root <davisampaio.sann@gmail.com>
2025-04-08 22:26:03 -0300

    melhorando o componente LogoItens

--- components/LogoItens.js
+++ components/LogoItens.js
@@ -1,32 +1,33 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import { Image, Text, View, StyleSheet } from 'react-native';
 
-const logoItens = ({item})=> {
-  return(
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems:'center',
-        margin:10,
-        }}>     
-    <Image source={item.uri}
-       style={{
-           width: item.width,
-           height: item.height, 
-           borderRadius: item.width / 2,
-           marginRight: 10,
-          }}
-      />
-
-    <Text style={{
-         color: 'yellow',
-         fontSize: 18,
-         fontWeight:'bold',
-         textAlign: 'left',
-         marginBottom:22,
-        }}> {item.nome} </Text>
-     </View>
+const LogoItens = ({ item: { uri, nome, width, height } }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={uri} style={[styles.image, { width, height, borderRadius: width / 2 }]} />
+      <Text style={styles.text}>{nome}</Text>
+    </View>
   );
 };
-export default logoItens;
+
+// Estilos extraídos para o StyleSheet
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: 10,
+  },
+  image: {
+    marginRight: 10,
+  },
+  text: {
+    color: 'yellow',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginBottom: 22,
+  },
+});
+
+export default LogoItens;
